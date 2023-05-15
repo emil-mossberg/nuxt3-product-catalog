b2a2b46edcc6a023970cfb83828d8719e1cfa30f
Emil Mossberg <emil.mossberg@vaimo.com>
2023-05-15 09:04:37 +0200

    Added minimal support for PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,43 @@
 
 export default defineNuxtConfig({
   css: ["~/assets/styles/main.less", "~/assets/styles/typography.less"],
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@vite-pwa/nuxt"],
   pinia: {
     autoImports: ["defineStore", "storeToRefs"],
+  },
+  pwa: {
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,ico,png,svg, woff2}"],
+    },
+    registerType: "prompt", // This is default, how can I test this and other which is "autoUpdate"
+    manifest: {
+      name: "Lantmännen Lantbruks Produktkatalog",
+      short_name: "LM Produktkatalog",
+      description: "Sida för att söka bland Lantmännen Lantbruks produkter",
+      theme_color: "#1E6E37",
+      display: "standalone",
+      icons: [
+        {
+          src: "android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "maskable_icon-140x140.png",
+          sizes: "140x140",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+    devOptions: {
+      enabled: false,
+    },
   },
   imports: {
     dirs: ["stores"],
